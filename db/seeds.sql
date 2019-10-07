2eefb697d3c24d64d16a568d86c271d65e97331d
Drew Reynolds <drew@sportscrm.com>
2019-10-06 21:00:23 -0500

    basic file setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', FALSE);
